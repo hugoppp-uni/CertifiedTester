69fc63ec83db15dabed65f373520ca8080e6c67c
Hugo Protsch <hugo.protsch@gmail.com>
2022-05-27 15:18:01 +0200

    Add test for exc2

--- src/test/kotlin/algorithms/MMBUeTest.kt
+++ src/test/kotlin/algorithms/MMBUeTest.kt
@@ -8,11 +8,21 @@ import kotlin.test.assertEquals
 internal class MMBUeTest {
 
     @Test
-    fun run() {
+    fun test_exc1() {
 
         val actualLines =
             filterInvalidCases(getResourceLines("exercise1.md"), CoverageType.MMBUe)
         val expectedLines = getResourceLines("exercise1_mmbue_solution.md")
+
+        assertEquals(expectedLines, actualLines)
+    }
+
+    @Test
+    fun test_exc2() {
+
+        val actualLines =
+            filterInvalidCases(getResourceLines("exercise2.md"), CoverageType.MMBUe)
+        val expectedLines = getResourceLines("exercise2_mmbue_solution.md")
 
         assertEquals(expectedLines, actualLines)
     }
