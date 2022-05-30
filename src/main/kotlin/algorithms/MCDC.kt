d00ff8ea71b613929f5be3c9bec11f773ce861d4
Minh Lê <minh.le@haw-hamburg.de>
2022-05-30 17:40:26 +0200

    Add MCDC#testPairsForColumn
    the function will be implemented according to a drawio drawing

--- src/main/kotlin/algorithms/MCDC.kt
+++ src/main/kotlin/algorithms/MCDC.kt
@@ -8,4 +8,17 @@ class MCDC : Algorithm {
         // select appropriate testcases, and try to minimize their number
         return setOf()
     }
+
+    /**
+     *
+     * @param table decision table
+     * @param columnIndex index of the column, for which the significant testcases will be searched
+     * @return a set of testcase pairs, that are needed for MC/DC coverage
+     */
+    private fun testPairsOfColumns(table: DecisionTable, columnIndex: Int): Set<Pair<Int, Int>> {
+        val indicesTodo = (0 until table.Rows.size).toMutableList()
+        val pairs = mutableSetOf<Pair<Int, Int>>()
+
+        return pairs
+    }
 }
