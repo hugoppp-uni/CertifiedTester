72883e46ce35771781e932d6d32fedebd62bd0b2
Minh Lê <minh.le@haw-hamburg.de>
2022-05-30 17:42:29 +0200

    Update MCDC#testPairsForColumn

--- src/main/kotlin/algorithms/MCDC.kt
+++ src/main/kotlin/algorithms/MCDC.kt
@@ -19,6 +19,28 @@ class MCDC : Algorithm {
         val indicesTodo = (0 until table.Rows.size).toMutableList()
         val pairs = mutableSetOf<Pair<Int, Int>>()
 
+        // generating pairs (first, second)
+        while (indicesTodo.isNotEmpty()) {
+            val firstIndex = indicesTodo.removeFirst()
+            val firstEntry = table.Rows[firstIndex]
+            val firstEntryWithoutCol = firstEntry.withoutConditionAt(columnIndex)
+
+            // search through remaining rows, to find partner test
+            for (secondIndex in indicesTodo) {
+                val secondEntry = table.Rows[secondIndex]
+
+                // partner was found with significant change
+                if (firstEntryWithoutCol == secondEntry.withoutConditionAt(columnIndex)
+                    && firstEntry.evaluated != secondEntry.evaluated
+                ) {
+                    indicesTodo.remove(secondIndex)
+                    pairs.add(Pair(firstIndex, secondIndex))
+                    break
+                }
+            }
+            // todo throw exception if no partner test was ever found
+            //  it shows that the input decision table was not complete
+        }
         return pairs
     }
 }
