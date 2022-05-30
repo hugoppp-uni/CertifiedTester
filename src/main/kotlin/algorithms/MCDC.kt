0ab08c2875054430b0cba12de83cb0bf9ff13e2f
Minh Lê <minh.le@haw-hamburg.de>
2022-05-30 17:37:09 +0200

    Update MCDC
    working on clean slate

--- src/main/kotlin/algorithms/MCDC.kt
+++ src/main/kotlin/algorithms/MCDC.kt
@@ -3,13 +3,9 @@ package algorithms
 import DecisionTable
 
 class MCDC : Algorithm {
-    override fun run(input: DecisionTable): Set<Int> {
-
-        //example access
-        var subExpression0 = input.Rows[0].subExpressions[0]
-        var evaluated = input.Rows[0].evaluated
-
-        TODO("Not yet implemented")
+    override fun run(decisionTable: DecisionTable): Set<Int> {
+        // find testcase pairs for each column/condition
+        // select appropriate testcases, and try to minimize their number
+        return setOf()
     }
-
 }
