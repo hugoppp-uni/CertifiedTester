531888f8ad894f8bf0639cc9e500b0321001ebc6
jonathanscherer <jonathan.scherer1@yahoo.de>
2022-05-31 14:33:28 +0200

    solutions + Tests

--- src/test/kotlin/algorithms/MCDCTest.kt
+++ src/test/kotlin/algorithms/MCDCTest.kt
@@ -4,6 +4,7 @@ import filterInvalidCases
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class MCDCTest {
     @Test
@@ -22,8 +23,14 @@ class MCDCTest {
         val actualLines =
             filterInvalidCases(getResourceLines("exercise2.md"), CoverageType.MCDC)
         val expectedLines = getResourceLines("exercise2_mcdc_solution.md")
+        val expectedLines2 = getResourceLines("exercise2_mcdc_solution2.md")
+        val expectedLines3 = getResourceLines("exercise2_mcdc_solution3.md")
+        val expectedLines4 = getResourceLines("exercise2_mcdc_solution4.md")
 
-        assertEquals(expectedLines, actualLines)
+    assertTrue { actualLines.equals(expectedLines)||actualLines.equals(expectedLines2)||actualLines.equals(expectedLines3)||actualLines.equals(expectedLines4)}
+
+
+
     }
 
     private fun getResourceLines(path: String): List<String> {
